1b373ff4ec477bcccb8b2a1f0d33264624380ac8
Tom Cahill <me@tomcahill.co.uk>
2019-09-13 23:26:17 +0100

    - TWEAK: Written some data and read it back

--- buttress-db-worker.js
+++ buttress-db-worker.js
@@ -5,7 +5,8 @@ window.Buttress.Worker = () => {
 
   const tasks = {
     init: (payload) => __init(payload),
-    write: (payload) => __write(payload),
+    write: (payload) => __write(payload.collection, collection.item),
+    bulkWrite: (payload) => __bulkWrite(payload),
     readAll: (payload) => __readAll(payload),
   }
   
@@ -54,13 +55,13 @@ window.Buttress.Worker = () => {
     });
   };
 
-  const __write = (payload) => {
+  const __write = (collectionName, item) => {
     if (!db) return Promise.reject('DB isn\'t init');
   
     return new Promise((resolve, reject) => {
-      const tx = db.transaction(payload.collection, 'readwrite');
-      const collection = tx.objectStore(payload.collection);
-      collection.add(payload.item);
+      const tx = db.transaction(collectionName, 'readwrite');
+      const collection = tx.objectStore(collectionName);
+      collection.add(item);
       tx.oncomplete = () => {
         return resolve(true);
       };
@@ -69,6 +70,21 @@ window.Buttress.Worker = () => {
       };
     });
   };
+
+  const __bulkWrite = (payload) => {
+    if (!db) return Promise.reject('DB isn\'t init');
+
+    if (!payload.items) return Promise.reject('Items not part of bulk write payload');
+
+    console.log('__bulkWrite', payload.collection, payload.items.length);
+  
+    return new Promise((resolve, reject) => {
+      return payload.items.reduce((prev, item) => {
+        return prev.then(() => __write(payload.collection, item));
+      }, Promise.resolve())
+      .then(resolve);
+    });
+  }
 
   const __readAll = (payload) => {
     if (!db) return Promise.reject('DB isn\'t init');
