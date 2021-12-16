77f41e3acaa4945e17bbca97ccd2ff946f94c0e1
Tom Cahill <me@tomcahill.co.uk>
2021-12-16 12:07:13 +0000

    FIX: Process add/delete before updates in bulk requests

--- buttress-db-data-service.js
+++ buttress-db-data-service.js
@@ -344,7 +344,7 @@ export default class ButtressDbDataService extends PolymerElement {
     if (this.get('logging')) console.log(`remove rq: ${entityId}`);
 
     return this.__queueRequest({
-      type: 'rm',
+      type: 'delete',
       url: this.scalarBaseUrl(entityId),
       entityId: entityId,
       method: 'DELETE',
@@ -408,16 +408,8 @@ export default class ButtressDbDataService extends PolymerElement {
     const settings = this.get('settings');
     this.status = 'working';
 
-    // Possible race condition if you between updates & adds, if a update gets in
-    // before an add it will try to grab future updates into it's request see below:
-    // - Update to object A
-    // - Add Object B
-    // - Update Object B
-
-    // process 'add's before updates,
-    // break chunk if we hit an 'add' between updates,
-    // add batch/ endpoint instead so we can just bulk process requests instead of having bulk/* endpoints
-    let request = this.requestQueue.shift();
+    const requestIdx = this.requestQueue.findIndex((r) => r.type === 'add' || r.type === 'delete');
+    let request = (requestIdx !== -1 && settings.bundled_requests) ? this.requestQueue.splice(requestIdx, 1).shift() : this.requestQueue.shift()
 
     // Attempt to reduce matching request types down to a single request
     if (settings.bundled_requests && ButtressDbDataService.constants.BUNDLED_REQUESTS_TYPES.includes(request.type)) {
